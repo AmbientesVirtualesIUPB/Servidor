d0181bc9203037d97b9eb6c7a5b88480fd8aba04
Julian Molina <judmolin@gmail.com>
2025-12-05 16:18:54 -0500

    llamado servidor manipular piezas

---
+++ Assets/_VE/Scripts/Motores/Servidor/ServidorMotores.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServidorMotores : MonoBehaviour
+{
+    public List<GuardarPieza> partes;
+    public Transform[] padresInstancia;
+    public bool esArmador = false;
+
+    public static ServidorMotores singleton;
+    private void Awake()
+    {
+        // Configurar Singleton
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GestionMensajesServidor.singeton.RegistrarAccion("MS00", InstanciaPiezaServidor);
+    }
+
+    public void InstanciaPiezaServidor(string msj)
+    {
+        PartesMotores parteMotor = JsonUtility.FromJson<PartesMotores>(msj);
+        InstanciarPieza(parteMotor);
+    }
+
+
+    public void InstanciarPieza(PartesMotores parte)
+    {
+        for (int i = 0; i < partes.Count; i++)
+        {
+            if (parte.id == partes[i].idPieza)
+            {
+                GameObject pieza = Instantiate(partes[i].gameObject,parte.pos,Quaternion.identity);
+                pieza.transform.parent = padresInstancia[pieza.GetComponent<GuardarPieza>().piezaExterna ? 0 : 1];
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class PartesMotores
+{
+    public int id;
+    public Vector3 pos;
+    public string idServidor;
+}
--- Assets/_VE/Scripts/Motores/UI/btnInventario.cs
+++ Assets/_VE/Scripts/Motores/UI/btnInventario.cs
@@ -43,10 +43,20 @@ public class btnInventario : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             // Aplicar desplazamiento en espacio local respecto al padre
             nuevaPieza.transform.localPosition += offsetLocal;
 
+            // INSTANCIA SERVIDOR
+            PartesMotores parte = new PartesMotores();
+            parte.id = idPieza;
+            parte.pos = nuevaPieza.transform.position;
+
+            MorionID morionID = nuevaPieza.GetComponent<MorionID>();
+            morionID.SetID(idPieza + "p" + Random.Range(1111, 9999));
+            morionID.isOwner = true;
+            parte.idServidor = morionID.GetID();
+
+            GestionMensajesServidor.singeton.EnviarMensaje("MS00", JsonUtility.ToJson(parte));
+
             InventarioUI.singleton.contadorInstancias -= 1; // Liberamos espacio en el inventario
             Destroy(this.gameObject); // Destruimos el boton
-
-            Debug.Log("se instanbcio la pieza con id: " + idPieza);
         }
         else
         {
