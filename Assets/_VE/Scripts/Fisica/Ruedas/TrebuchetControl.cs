86773139d710a2b10b97d1ccd38216dcf2a15a29
Morion <ambientesvirtuales@pascualbravo.edu.co>
2025-12-24 11:17:27 -0500

    Integrando nuevos elementos

--- Assets/_VE/Scripts/Fisica/Ruedas/TrebuchetControl.cs
+++ Assets/_VE/Scripts/Fisica/Ruedas/TrebuchetControl.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class TrebuchetControl : MonoBehaviour
 {
-    public float tiempoMov;
-    public int iteraciones = 200;
-    public Deslizante deslizante;
+    public float        tiempoMov;
+    public int          iteraciones = 200;
+    public Deslizante   deslizante;
+    public Transform    target;
 
     [ContextMenu("Mover")]
     public void Mover()
@@ -17,11 +18,20 @@ public class TrebuchetControl : MonoBehaviour
     IEnumerator Moviendo()
     {
         float t = 0;
-        for (int i = 0; i < 200; i++)
+        for (int i = 0; i < iteraciones; i++)
         {
             yield return new WaitForSeconds(tiempoMov / iteraciones);
             t += 1f / (float)iteraciones;
             deslizante.t = t;
+        }
+        GameObject go = new GameObject();
+        go.transform.position = transform.position;
+        go.transform.LookAt(target);
+        Quaternion q = transform.rotation;
+        for (int i = 0; i < 100; i++)
+        {
+            transform.rotation = Quaternion.Lerp(q, go.transform.rotation, i / 100f);
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
